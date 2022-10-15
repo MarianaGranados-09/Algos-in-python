899fcbe03ca494d49a161f32db7d039f2723ad40
Mariana Granados <mgranados09@alumnos.uaq.mx>
2022-10-15 17:19:44 -0500

    Big O notation theory
    Understanding how the insertion sort algorithm runs in O(n^2).

---
+++ bigO.py
@@ -0,0 +1,8 @@
+#Big O Notation theory
+
+# In the case of insertion sort, when we look at our expression for the max number of steps required to perform the algorithm, we find that its a sum of two terms: one is a multiple of n^2 and the other is a multiple of n.
+# the term that is a multiple of n will matter less and
+# less as n grows, and the n2 term will come to be the only one that we are
+# concerned with. So the worst case of insertion sort is that it is a O(n^2) 
+
+#We can use big O notation to talk about time but also about space
