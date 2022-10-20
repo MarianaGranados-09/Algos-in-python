1edca110fa82cdca1e39c856248591b09d667ad0
Mariana Granados <mgranados09@alumnos.uaq.mx>
2022-10-20 03:02:43 -0500

    graph theory and breadth-first search (BFS) algo

---
+++ graphs.py
@@ -0,0 +1,28 @@
+#graphs- first graph algo called breadth-first search (BFS)
+
+#Allows you to find the shortest distance between two things, the algo can
+#be used for:
+
+#Write a checkers AI that calculates the fewest moves to victory
+#Write a spell checker
+#Find the doctor closest to you in your network
+
+#shortest-path problem
+#1. model the problem as a graph
+#2. solve the problem using breadth-first search
+
+#What is a graph? A graph models a set of connections, each node is made
+#up of nodes and edges, a node can be connected to many other nodes called 
+#neighbors. 
+
+#Breadth-first search:
+#1. is there a path from node A to node B?
+#2. What is the shortest path from node A to node B?
+
+#Queues: are similar to stacks, you can't access random elements
+#in the queue, instead, there are only 2 operations: enqueue and dequeue
+
+#enqueue: add an item to the queue, dequeue: take an item off the queue
+
+#queue: FIFO ds - first in, first out
+#stack: LIFO ds - last in, first out
