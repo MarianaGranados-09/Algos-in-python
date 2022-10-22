f47b32e8328d6b2c0d7a3168db6e4633d845d183
Mariana Granados <mgranados09@alumnos.uaq.mx>
2022-10-22 02:06:37 -0500

    notes on dynamic programming

---
+++ dynamic.py
@@ -0,0 +1,15 @@
+#dynamic programming is a technique used to solve a hard problem by
+#breaking it up into subproblems and solving those subproblems first
+
+#checking every set posible of goods will take o(2^n) time
+
+#solving problem for smaller knapsnacks and then work up to solving the original
+#problem, dynamic programming is a hard concept...
+
+#every dynamic programming algo starts with a grid
+#one row for each item to choose from-3
+#columns for knapback sizes from 1 to 4 pounds - 4
+
+#in dynamic programming, youre trying to maximize something
+#trying to optimize something given a constraint
+#every solution involves a grid
