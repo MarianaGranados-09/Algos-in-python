64ff446ce46640b96138348eb2235c2810a3b1fe
Mariana Granados <mgranados09@alumnos.uaq.mx>
2022-10-22 02:19:53 -0500

    notes on common ML algorithms

---
+++ KNN.py
@@ -0,0 +1,23 @@
+#k-nearest neighbors
+#classification system using the k-nearest neighbors algo
+
+#classification = categorization into a group
+#regression = predicitng a response (like a number)
+
+#Intro to machine learning through KNN
+
+#OCR stands for optical character recognition, means taking a photo
+#of a page of text and the computer automatically read the text for u
+
+#How does OCR work? Using KNN
+#1. Go through a lot of images of numbers, extract features of those
+#numbers
+#2. When u get a new image, extract the features of that image and
+#see what its neareast neighbors are.
+
+#Generally speaking, OCR algo measures lines, points and curves.
+#The first step of OCR is called training, most ML algos have a training step
+
+#Building a spam filter
+#use another algo called the naive bayes classifier, first you train your
+#naive bayes classifier on some data
